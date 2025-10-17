b0dd35c53d322267a0360977f9cf1da9f5cec549
Kevin Ge <ge.kevin.a@gmail.com>
2025-10-17 14:29:24 -0500

    docs: add comments to valve logic

--- control/src/valve.h
+++ control/src/valve.h
@@ -8,8 +8,11 @@ enum class Valve {
   kValveMax
 };
 
+// Set up underlying valve GPIO pins, pwm timer, GPIO -> channel mapping.
 void setup_valves();
 
+// Open valve to configured `open_angle`. See configs/valve_config.h.
 void open_valve(Valve valve);
 
+// Close valve to configured `close_angle`. See configs/valve_config.h.
 void close_valve(Valve valve);
