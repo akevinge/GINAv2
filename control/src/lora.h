bc079c947e5874e9441361bef49b287e8762304b
Michael Hellman <michaelhellmaniii@gmail.com>
2025-11-07 12:28:22 -0600

    FEAT: LoRa transmission of telemetry data

--- control/src/lora.h
+++ control/src/lora.h
@@ -1,11 +1,28 @@
 #ifndef LORA_H_
 #define LORA_H_
-
-void demo_main();
+#include "pt.h"
+#include "sensor_management.h"
 
 typedef struct {
-    float load_vout;
-    uint16_t pt_reading;
+    sensor_data_t sensor_data;
+    TickType_t timestamp;
 } telemetry_t;
+
+void configure_lora();
+#ifdef CONFIG_AWAY_SENDER
+void away_tx_task(void *pvParameters);
+#endif // CONFIG_AWAY_SENDER
+
+#ifdef CONFIG_AWAY_RECEIVER
+void away_rx_task(void *pvParameters);
+#endif // CONFIG_AWAY_RECEIVER
+
+#ifdef CONFIG_HOME_SENDER
+void home_tx_task(void *pvParameters);
+#endif // CONFIG_HOME_SENDER
+
+#ifdef CONFIG_HOME_RECEIVER
+void home_rx_task(void *pvParameters);
+#endif // CONFIG_HOME_RECEIVER
 
 #endif  // LORA_H_
