063833ea1129f16d6b598b55b8921145fbc3f4b1
Michael Hellman <michaelhellmaniii@gmail.com>
2025-11-12 17:40:50 -0600

    MISC: misc changes for future

--- control/src/main.cc
+++ control/src/main.cc
@@ -14,19 +14,18 @@ static const char *TAG = "MAIN";
 #ifdef CONFIG_AWAY_SENDER
 void poll_sensor_task(void* pvParameters) {
   QueueHandle_t sensor_queue = static_cast<QueueHandle_t>(pvParameters);
+  TickType_t xLastWakeTime = xTaskGetTickCount();
   while (1) {
     sensor_data_t *data = new sensor_data_t();
     for (int i = 0; i < static_cast<int>(Pt::kPtMax); ++i) {
       //data->pt_readings[i] = read_pt_int(static_cast<Pt>(i));
       data->pt_readings[i] = i;  // Dummy data for testing
     }
-    ESP_LOGI(TAG, "Polled PT readings");
     data->load_cell_reading = 0;
     data->timestamp = xTaskGetTickCount();
     // Send sensor data to the queue
-    xQueueSendToFront(sensor_queue, &data, portMAX_DELAY); // Send to front, we want to prioritize latest data
-
-    vTaskDelay(pdMS_TO_TICKS(1000 / SENSOR_SAMPLE_RATE_HZ));
+    xQueueSendToFront(sensor_queue, data, portMAX_DELAY); // Send to front, we want to prioritize latest data
+    vTaskDelayUntil(&xLastWakeTime, SENSOR_SAMPLE_RATE_TICKS);
   }
 }
 #endif // CONFIG_AWAY_SENDER
@@ -39,8 +38,8 @@ void setup_lora_tasks(){
     sensor_queue = xQueueCreate(50, sizeof(sensor_data_t)); // Queue to hold sensor data
 
     configure_lora();
-    xTaskCreatePinnedToCore(poll_sensor_task, "Poll_Sensor_Task", 4096, (void*)sensor_queue, 5, NULL, tskIDLE_PRIORITY);
-    xTaskCreatePinnedToCore(away_tx_task, "LoRa_TX_Task", 8192, (void*)sensor_queue, 5, NULL, 1);
+    xTaskCreatePinnedToCore(poll_sensor_task, "Poll_Sensor_Task", 4096, (void*)sensor_queue, 1, NULL, tskNO_AFFINITY);
+    xTaskCreatePinnedToCore(away_tx_task, "LoRa_TX_Task", 8192, (void*)sensor_queue, 5, NULL, tskNO_AFFINITY);
   #endif // CONFIG_AWAY_SENDER
   #ifdef CONFIG_HOME_RECEIVER
     ESP_LOGI(TAG, "Starting Home Receiver Configuration");
