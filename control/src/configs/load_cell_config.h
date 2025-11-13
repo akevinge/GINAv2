7e670ab5410edd3e20f27f5c5bb8f54659772394
Kevin Ge <ge.kevin.a@gmail.com>
2025-11-13 10:26:56 -0600

    feat: add load cell hx711 code

---
+++ control/src/configs/load_cell_config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <driver/gpio.h>
+#include <hx711.h>
+
+// Pin configuration for the HX711 load cell amplifier.
+constexpr gpio_num_t HX711_DOUT_GPIO_NUM = GPIO_NUM_48;
+constexpr gpio_num_t HX711_PD_SCK_GPIO_NUM = GPIO_NUM_47;
+constexpr hx711_gain_t HX711_GAIN = HX711_GAIN_A_128;
+
+// How long to wait for the HX711 to become ready.
+constexpr int HX711_MAX_TIMEOUT_MS = 500;
+
+// Number of samples to average when reading from the HX711.
+constexpr int HX711_AVG_SAMPLE_COUNT = 5;
