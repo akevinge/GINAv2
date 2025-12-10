cf6b9f414902139338e8094d8c140fbd004fd377
Kevin Ge <ge.kevin.a@gmail.com>
2025-12-09 21:34:07 -0600

    fix: ignition relay pin

--- control/src/configs/ignition_config.h
+++ control/src/configs/ignition_config.h
@@ -2,5 +2,6 @@
 
 #include <driver/gpio.h>
 
-constexpr gpio_num_t IGNITION_GPIO_NUM = GPIO_NUM_1;
-constexpr int IGNITION_RELAY_PULSE_MS = 5000;  // Pulse duration to activate igniter
+constexpr gpio_num_t IGNITION_GPIO_NUM = GPIO_NUM_37;
+constexpr int IGNITION_RELAY_PULSE_MS =
+    5000;  // Pulse duration to activate igniter
